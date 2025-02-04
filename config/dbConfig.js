f00036a26a9ccb772966f25a4f71eaa4d73e2131
ankankumar123 <ankankumar257@gmail.com>
2025-02-04 17:04:44 +0530

    Initial commit

---
+++ config/dbConfig.js
@@ -0,0 +1,19 @@
+const mongoose=require('mongoose');
+
+
+mongoose.connect(process.env.mongo_url,
+//     {
+//     dbName:"zebrakat",
+//    }
+);
+  
+const connection = mongoose.connection;
+
+connection.on('error',()=>{
+console.log("error connecting to Database");
+})
+connection.on('connected',()=>{
+    console.log("dataBase connection established");
+})
+
+module.exports=connection;
